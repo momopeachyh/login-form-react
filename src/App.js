5fafed8814877bfc5f38bfb409c1e556194f70ad
Heidi <heidi.willden@me.com>
2022-03-22 17:21:13 -0500

    Imported react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Login from "./Login.js";
 import Success from "./Success.js";
 import Failure from "./Failure.js";
@@ -6,9 +7,11 @@ import Failure from "./Failure.js";
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Success />
-      <Failure />{" "}
+      <Routes>
+        <Route path="/login" component={Login} />
+        <Route path="/success" component={Success} />
+        <Route path="/failure" component={Failure} />
+      </Routes>
     </div>
   );
 }
