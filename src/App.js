90dd3611c2ace1a78bd5b1cccc8de108e80f462a
Heidi <heidi.willden@me.com>
2022-03-22 15:17:20 -0500

    Initial layout and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <label>Username</label>
+        <input type="text" className="form-control mb-3"></input>
+        <label>Password</label>
+        <input type="text" className="form-control mb-3"></input>
+        <label>Email</label>
+        <input type="email" className="form-control mb-3"></input>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
