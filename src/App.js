8b92055b69deb2f02e5593b7b83c69439ff5dda8
Heidi <heidi.willden@me.com>
2022-03-23 16:39:41 -0500

    Imported hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login.js";
 import Success from "./Success.js";
 import Failure from "./Failure.js";
